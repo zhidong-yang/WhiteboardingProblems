24e892a0d4021255697d4c59d83ff24fc289af0f
zhidong-yang <zhidong.yang@outlook.com>
2020-09-06 18:39:15 -0400

    Practice Sep. 6

---
+++ WhiteboardingProblems/September2020/Sep6.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteboardingProblems.September2020
+{
+    class Sep6
+    {
+    }
+}
