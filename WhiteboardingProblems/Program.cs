1139cddd21d893b6547579959e0a525e1af0019c
zhidong-yang <zhidong.yang@outlook.com>
2020-09-11 14:40:00 -0400

    Sep.11 question removing strings from numbers in a list

--- WhiteboardingProblems/Program.cs
+++ WhiteboardingProblems/Program.cs
@@ -11,10 +11,9 @@ namespace WhiteboardingProblems
     {
         static void Main(string[] args)
         {
-            int[] arrayOfNum = { 3, 14, 26, 28, 7, 2, 33, 27, 33, 6, 45, 15, 14, 13, 2, 6, 11, 5, 30, 45, 20, 31, 22, 39, 31, 24, 8 };
-            int n = 5;
+            object o1 = 650;
 
-            Console.WriteLine(Sep4.QueueTime(arrayOfNum, n));
+            Console.WriteLine(o1.GetType());
             Console.ReadLine();
         }
 
